bbb5114c9a03e8192843e53e579b6607f26539ae
naphatrapee <6631501157@lamduan.mfu.ac.th>
2025-11-12 15:45:58 +0700

    update App_theme to themeData

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Room Reservation',
-      theme: AppTheme.lightTheme,
+      theme: AppTheme.themeData,
       home: SignInScreen(),
       debugShowCheckedModeBanner: false,
     );
